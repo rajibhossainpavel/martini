1bb05564226790df14d56c8023a87a74cd8e598c
Rajib Hossain Pavel <39058080+rajibhossainpavel@users.noreply.github.com>
2022-06-06 23:47:49 +0600

    init!

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,18 +1,43 @@
-import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { ObjectType, Field, Float, Int, ID } from '@nestjs/graphql';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 @ObjectType()
 export class ItemType {
   @Field(() => ID)
   @IsString()
    id?: string;
+
   @Field()
   @IsString()
   @IsNotEmpty()
-   title: string;
-  @Field(() => Int)
-  @IsNumber()
-   price: number;
+   name: string;
+  
   @Field()
   @IsString()
+  @IsNotEmpty()
+  categoy_name: string;
+  
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  parent_categoy: string;
+  
+  @Field(() => Float)
+  @IsNumber()
+   price: number;
+  
+   @Field()
+  @IsString()
    description: string;
+
+  @Field(() => Int)
+  @IsNumber()
+  parent_num: number;
+
+  @Field(() => Int)
+  @IsNumber()
+  left_num: number;
+     
+  @Field(() => Int)
+  @IsNumber()
+  right_num: number;
 }
